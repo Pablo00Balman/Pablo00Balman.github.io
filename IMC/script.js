38407c2f88c5750c19fb13f07bf4f20112b442a1
Pablo Balman <pablobbalman@gmail.com>
2021-10-10 20:12:05 -0300

    Novo site IMC

--- IMC/script.js
+++ IMC/script.js
@@ -9,27 +9,27 @@ function IMC()
 
     if(R> 40)
     {
-        txt = nome+", você está com obesidade mórbida :( . --- IMC = "+R.toFixed(1);
+        txt = nome+", você está com obesidade mórbida :( . - IMC = "+R.toFixed(1);
     }
     else if(R<39.9 && R>=35)
     {
-        txt = nome+", você está com Obesidade Grau II. --- IMC = "+R.toFixed(1);    
+        txt = nome+", você está com Obesidade Grau II. - IMC = "+R.toFixed(1);    
     }
     else if(R<34.9 && R>=30)
     {
-        txt = nome+", você está com Obesidade Grau I. --- IMC = "+R.toFixed(1);
+        txt = nome+", você está com Obesidade Grau I. - IMC = "+R.toFixed(1);
     }
     else if(R< 29.9 && R>=25)
     {
-        txt = nome+", você com sobrepeso.  --- IMC = "+R.toFixed(1);
+        txt = nome+", você está com sobrepeso.  - IMC = "+R.toFixed(1);
     }
     else if(R<24.9 && R>=18.5)
     {
-        txt = nome+", você com seu peso normal. --- IMC = "+R.toFixed(1);    
+        txt = nome+", você está com seu peso normal. - IMC = "+R.toFixed(1);    
     }
     else if(R>18.5)
     {
-        txt = nome+", você está abaixo do peso. --- IMC = "+R.toFixed(1);
+        txt = nome+", você está abaixo do peso. - IMC = "+R.toFixed(1);
     }
     else{
         txt = "ERRO, favor, recarregue o site tente novamente."; 
