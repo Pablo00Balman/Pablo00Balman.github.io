cecc8f5c37d894eabe5f4f1efbcf82e86ed365e9
Pablo Balman <pablobbalman@gmail.com>
2021-10-10 20:09:17 -0300

    Novo site IMC

---
+++ IMC/script.js
@@ -0,0 +1,40 @@
+function IMC()
+{
+    let nome = document.getElementById("A").value;
+    let peso = document.getElementById("B").value;
+    let altura = document.getElementById("C").value;
+    
+    let R = peso/ (altura*altura);
+    let txt = "";
+
+    if(R> 40)
+    {
+        txt = nome+", você está com obesidade mórbida :( . --- IMC = "+R.toFixed(1);
+    }
+    else if(R<39.9 && R>=35)
+    {
+        txt = nome+", você está com Obesidade Grau II. --- IMC = "+R.toFixed(1);    
+    }
+    else if(R<34.9 && R>=30)
+    {
+        txt = nome+", você está com Obesidade Grau I. --- IMC = "+R.toFixed(1);
+    }
+    else if(R< 29.9 && R>=25)
+    {
+        txt = nome+", você com sobrepeso.  --- IMC = "+R.toFixed(1);
+    }
+    else if(R<24.9 && R>=18.5)
+    {
+        txt = nome+", você com seu peso normal. --- IMC = "+R.toFixed(1);    
+    }
+    else if(R>18.5)
+    {
+        txt = nome+", você está abaixo do peso. --- IMC = "+R.toFixed(1);
+    }
+    else{
+        txt = "ERRO, favor, recarregue o site tente novamente."; 
+    }
+       
+    document.getElementById("resultado").value = txt;
+
+}
