519f8fd02102faad491391fe65e757fc366ef3b5
Pablo Balman <pablobbalman@gmail.com>
2021-11-23 21:28:49 -0300

    Enviando tudo

--- WEB Storage/js/view/indexView.js
+++ WEB Storage/js/view/indexView.js
@@ -3,44 +3,21 @@
  * o seu modelo de dados na tela
  * 
  */
- 
+
 /**
  * Define como os itens serão carregados na tela
  * @param {lista de itens} foods 
  */
-
-/*  function createItems(food) {
+ function createItems(foods) {
     //recupera o container dos itens
     let menu = document.querySelector("#menu");
     //apagando os itens atuais do DOM
     menu.innerHTML = "";
-    food.forEach(food => {
-        let divFood = document.createElement("figure");
-
-        divFood.innerHTML = `<button id = "update = ${food.id}">Editar</button>
-        <figure id = "food-${food.id}">
-            <img src = "${food.image}" alt = ${food.name}">
-            <figcaption> ${food.name}</figcaption>
-            </figure>`;
-
-        menu.appendChild(divFood);
-    });
-
-} */
-
-function createItems(food) {
-    //recupera o container dos itens
-    let menu = document.querySelector("#menu");
-    //apagando os itens atuais do DOM
-    menu.innerHTML = "";
-    food.forEach(food => {
+    foods.forEach(food => {
         let figFood = document.createElement("figure");
-        figFood.id = food.id;
-
-        figFood.innerHTML =`
-                <button id = "update = ${food.id}">Editar</button>
-                <img src="${food.image}" alt="${food.name}">
-                <figcaption>${food.name}</figcaption>`;
+        figFood.id = `food-${food.id}`;
+        figFood.innerHTML =`<button>Editar</button><img src="${food.image}" alt="${food.name}">
+                <figcaption>${food.name}</figcaption>` ;
         menu.appendChild(figFood);
     });
 }
