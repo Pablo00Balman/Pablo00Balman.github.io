2a0621ee4522a3d3f13d096204d404df78ded5d1
Pablo Balman <61887796+Pablo00Balman@users.noreply.github.com>
2023-07-14 14:56:41 -0300

    Add files via upload

---
+++ instalarPop.bat
@@ -0,0 +1,13 @@
+@echo off
+
+start "" https://iso.pop-os.org/22.04/amd64/intel/32/pop-os_22.04_amd64_intel_32.iso
+
+
+diskpart
+select disk 1
+clean
+create part pri 
+select part 1
+format fs=fat32 quick
+exit
+
